f3a8734e94aa04a9e043744d4a1a51d0ebbd0dde
Jose Manuel Abuin Mosquera <abuinjm@gmail.com>
2016-04-15 17:17:30 +0200

    Updated version 0.0.2 -> 0.1.0

--- MM-Suite.c
+++ MM-Suite.c
@@ -10,7 +10,7 @@
 #include "solvers/ConjugateGradient.h"
 
 #ifndef PACKAGE_VERSION
-#define PACKAGE_VERSION "0.0.2"
+#define PACKAGE_VERSION "0.1.0"
 #endif
 
 
--- MPI/MM-Suite-MPI.c
+++ MPI/MM-Suite-MPI.c
@@ -11,7 +11,7 @@
 #include "solvers/ConjugateGradient-MPI.h"
 
 #ifndef PACKAGE_VERSION
-#define PACKAGE_VERSION "0.0.2"
+#define PACKAGE_VERSION "0.1.0"
 #endif
 
 
