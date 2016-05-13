595f7f4b43244e9e08019905fdbdf00ed65db42b
Jose Manuel Abuin Mosquera <abuinjm@gmail.com>
2016-05-13 09:13:16 +0200

    Added functions to get the upper, lower and diagonal values of a matrix

---
+++ utils/LUValues.c
@@ -0,0 +1,55 @@
+#include "LUValues.h"
+
+void getLValues(double *L, double *values,unsigned long M,unsigned long N, unsigned long long nz) {
+
+	for(int i= 0; i< M; i++){
+	
+		for(int j = 0; j<N;j++){
+		
+			if(i<j){
+				L[i*N+j] = values[i*N+j];
+			}
+			else{
+				L[i*N+j] = 0.0;
+			}
+		
+		}
+	}
+
+}
+
+void getUValues(double *U, double *values,unsigned long M,unsigned long N, unsigned long long nz) {
+
+	for(int i= 0; i< M; i++){
+	
+		for(int j = 0; j<N;j++){
+		
+			if(i>j){
+				U[i*N+j] = values[i*N+j];
+			}
+			else{
+				U[i*N+j] = 0.0;
+			}
+		
+		}
+	}
+
+}
+
+void getDValues(double *D, double *values,unsigned long M,unsigned long N, unsigned long long nz) {
+
+	for(int i= 0; i< M; i++){
+	
+		for(int j = 0; j<N;j++){
+		
+			if(i==j){
+				D[i*N+j] = values[i*N+j];
+			}
+			else{
+				D[i*N+j] = 0.0;
+			}
+		
+		}
+	}
+
+}
---
+++ utils/LUValues.h
@@ -0,0 +1,5 @@
+
+
+void getLValues(double *L, double *values,unsigned long M,unsigned long N, unsigned long long nz);
+void getUValues(double *U, double *values,unsigned long M,unsigned long N, unsigned long long nz);
+void getDValues(double *D, double *values,unsigned long M,unsigned long N, unsigned long long nz);
